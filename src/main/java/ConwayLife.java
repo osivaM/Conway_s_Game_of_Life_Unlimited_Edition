71fb275903bf15c3d0efcc0bd48f0e5b2bc93a3d
osivaM <o.v.v.9322@gmail.com>
2023-09-29 17:42:11 +0500

    fourth commit Final version

--- src/main/java/ConwayLife.java
+++ src/main/java/ConwayLife.java
@@ -1,66 +1,32 @@
 public class ConwayLife {
     public static int[][] getGeneration(int[][] cells, int generations) {
-        int[][] result = new int[cells.length][cells[0].length];
-
-        for (int i = 0; i < cells.length; i++) {
-            System.arraycopy(cells[i], 0, result[i], 0, cells[i].length);
-        }
-
         for (int count = 0; count < generations; count++) {
-            int[][] temp = new int[result.length + 4][result[0].length + 4];
+            int[][] temp = new int[cells.length + 4][cells[0].length + 4];
             int[][] interRes = new int[temp.length][temp[0].length];
-            int up, down, left, right;
-            boolean firstOne;
+            int[] neighbors = {-1, -1, -1, 0, -1, 1, 1, -1, 1, 0, 1, 1, 0, -1, 0, 1};
+            int up = 0, down = 0, left = interRes.length, right = 0;
+            boolean firstOne = false;
 
-            for (int i = 0; i < result.length; i++) {
-                System.arraycopy(result[i], 0, temp[i + 2], 2, result[i].length);
+            for (int i = 0; i < cells.length; i++) {
+                System.arraycopy(cells[i], 0, temp[i + 2], 2, cells[i].length);
             }
 
             for (int i = 1; i < temp.length - 1; i++) {
                 for (int j = 1; j < temp[i].length - 1; j++) {
                     int liveCell = 0;
 
-                    if (temp[i - 1][j - 1] == 1) {
-                        liveCell++;
-                    }
-                    if (temp[i - 1][j] == 1) {
-                        liveCell++;
-                    }
-                    if (temp[i - 1][j + 1] == 1) {
-                        liveCell++;
-                    }
-                    if (temp[i + 1][j - 1] == 1) {
-                        liveCell++;
-                    }
-                    if (temp[i + 1][j] == 1) {
-                        liveCell++;
-                    }
-                    if (temp[i + 1][j + 1] == 1) {
-                        liveCell++;
-                    }
-                    if (temp[i][j - 1] == 1) {
-                        liveCell++;
-                    }
-                    if (temp[i][j + 1] == 1) {
-                        liveCell++;
+                    for (int k = 0; k < 16; k += 2) {
+                        if (temp[i + neighbors[k]][j + neighbors[k + 1]] == 1) {
+                            liveCell++;
+                        }
                     }
 
                     if (temp[i][j] == 0 && liveCell == 3) {
                         interRes[i][j] = 1;
-                    } else if (temp[i][j] == 1 && (liveCell < 2 || liveCell > 3)) {
-                        interRes[i][j] = 0;
-                    } else {
+                    } else if (liveCell >= 2 && liveCell <= 3) {
                         interRes[i][j] = temp[i][j];
                     }
-                }
-            }
 
-            left = interRes[0].length;
-            right = down = up = 0;
-            firstOne = false;
-            for (int i = 0; i < interRes.length; i++) {
-
-                for (int j = 0; j < interRes[0].length; j++) {
                     if (interRes[i][j] == 1) {
                         if (!firstOne) {
                             up = i;
@@ -79,12 +45,12 @@ public class ConwayLife {
                 }
             }
 
-            result = new int[down - up + 1][right - left + 1];
-            for (int i = 0; i < result.length; i++) {
-                System.arraycopy(interRes[i + up], left, result[i], 0, result[i].length);
+            cells = new int[down - up + 1][right - left + 1];
+            for (int i = 0; i < cells.length; i++) {
+                System.arraycopy(interRes[i + up], left, cells[i], 0, cells[i].length);
             }
         }
 
-        return result;
+        return cells;
     }
 }
